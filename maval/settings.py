e5bb9d96716e0ab7ae7ec5b3618b8cff1bd53e4a
christine0613 <35873307+christine0613@users.noreply.github.com>
2020-12-28 00:45:02 +0800

    Update settings.py

--- maval/settings.py
+++ maval/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ytv%accsqgrh8=w3p1m=pm4qwk5+!z0dsnykv^ogf986ksoksc'
+SECRET_KEY = 'your_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
